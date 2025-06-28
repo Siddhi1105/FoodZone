234147d7495c2a52f0430e74a4f1d5abc792f284
Siddhi1105 <siddhi.nanaware2004@gmail.com>
2025-06-28 22:56:11 +0530

    foodzone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './Landing/Landing.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Contact from './PageRouting/Contact.jsx';
+import Home from './PageRouting/Home.jsx';
+import Dine from './PageRouting/Dine.jsx';
+import Delivery from './PageRouting/Delivery.jsx';
+import Carts from './PageRouting/Carts.jsx';
+import Breakfast from './Menu/Breakfast.jsx';
+import Starters from './Menu/Starters.jsx';
+import Maincourse from './Menu/Maincourse.jsx';
+import Italian from './Menu/Italian.jsx'; 
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      
+      <Router>
+     
+     
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+         <Route path='/dine' element={<Dine/>}/>
+          <Route path='/delivery' element={<Delivery/>}/>
+           <Route path='/contact' element={<Contact/>}/>
+           <Route path='/carts' element={<Carts/>}/>
+        <Route path='/breakfast' element={<Breakfast/>}/>
+        <Route path='/starters' element={<Starters/>}/>
+        <Route path='/maincourse' element={<Maincourse/>}/>
+        <Route path='/italian' element={<Italian/>}/>
+
+      </Routes>
+       </Router>
+       
     </div>
   );
 }
